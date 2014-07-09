6b038904f7ca1fa1946802cac1078071121cd4b0
C Borkert <success@dpsw.info>
2014-07-09 16:02:50 -0500

    fix

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ Pretty print to console log, prints stdin if JSON cannot be parsed
 ````
 $ echo "{a:1,b:2}" | node -e "require('jutil').pretty()"
 ````
+
+Diff two JS objects
+````
+$ echo "[$ONE,$TWO]" | node -e "console.log(require('./jutil.js').diff())"
+````
--- jutil.js
+++ jutil.js
@@ -20,6 +20,89 @@ function pretty(obj){
   }
 }
 
+var deepDiffMapper = function() {
+    return {
+        VALUE_CREATED: '>',
+        VALUE_UPDATED: '>>',
+        VALUE_DELETED: '-',
+        VALUE_UNCHANGED: '=',
+        map: function(obj1, obj2) {
+            if (this.isFunction(obj1) || this.isFunction(obj2)) {
+                throw 'Invalid argument. Function given, object expected.';
+            }
+            if (this.isValue(obj1) || this.isValue(obj2)) {
+              var a = {};
+                a[this.compareValues(obj1, obj2)] = obj1 || obj2;
+                return a;
+            }
+            var diff = {};
+            for (var key in obj1) {
+                if (this.isFunction(obj1[key])) {
+                    continue;
+                }
+
+                var value2 = undefined;
+                if ('undefined' != typeof(obj2[key])) {
+                    value2 = obj2[key];
+                }
+
+                diff[key] = this.map(obj1[key], value2);
+            }
+            for (var key in obj2) {
+                if (this.isFunction(obj2[key]) || ('undefined' != typeof(diff[key]))) {
+                    continue;
+                }
+
+                diff[key] = this.map(undefined, obj2[key]);
+            }
+            return diff;
+
+        },
+        compareValues: function(value1, value2) {
+            if (value1 === value2) {
+                return this.VALUE_UNCHANGED;
+            }
+            if ('undefined' == typeof(value1)) {
+                return this.VALUE_CREATED;
+            }
+            if ('undefined' == typeof(value2)) {
+                return this.VALUE_DELETED;
+            }
+
+            return this.VALUE_UPDATED;
+        },
+        isFunction: function(obj) {
+            return toString.apply(obj) === '[object Function]';
+        },
+        isArray: function(obj) {
+            return toString.apply(obj) === '[object Array]';
+        },
+        isObject: function(obj) {
+            return toString.apply(obj) === '[object Object]';
+        },
+        isValue: function(obj) {
+            return !this.isObject(obj) && !this.isArray(obj);
+        }
+    }
+}();
+
 module.exports = {
-  pretty: pretty
+  pretty: pretty,
+  diff: function(a, b){
+    if(a && b){
+      if(typeof a === 'object')
+        a = JSON.stringify(a);
+      if(typeof b === 'object')
+        b = JSON.stringify(b);    
+      return deepDiffMapper.map(a, b);
+    }
+    var indata = '';
+    process.stdin.on('readable', function(){
+      indata += process.stdin.read() || '';
+    })
+    process.stdin.on('end', function(){
+      var arr = JSON.parse(indata);
+      console.log(deepDiffMapper.map(arr[0], arr[1]));
+    });
+  }
 }
